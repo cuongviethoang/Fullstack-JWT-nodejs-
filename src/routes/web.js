24ce1d77823396fd14d295d1aaa79b2c60fe30d4
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-02-14 10:41:08 +0700

    ket noi voi mysql

--- src/routes/web.js
+++ src/routes/web.js
@@ -10,7 +10,7 @@ const router = express.Router();
 const initWebRoutes = (app) => {
     router.get("/", homeController.handleHelloWorld);
     router.get("/user", homeController.handleUserPage);
-
+    router.post("/users/create-user", homeController.handleCreateNewUser);
     return app.use("/", router); // nạp tất cả router vào app
 };
 
--- src/server.js
+++ src/server.js
@@ -2,12 +2,18 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config(); // sử dụng câu lệnh này ta mới lấy được các biến trong file .env thông qua process.env
+import bodyParser from "body-parser";
 
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 
 // config view engine
 configViewEngine(app);
+
+//config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // init web routes
 initWebRoutes(app);
