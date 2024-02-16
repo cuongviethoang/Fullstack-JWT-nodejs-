5c4e9c98861308dd828e6b27689182563af21a82
cuongviethoang <hoangvietcuong2k2@gmail.com>
2024-02-16 21:33:03 +0700

    tao middleware cau hinh cors

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,6 +1,6 @@
 import express from "express";
 import homeController from "../controller/honeController";
-
+import apiController from "../controller/apiController";
 const router = express.Router();
 
 /**
@@ -14,6 +14,10 @@ const initWebRoutes = (app) => {
     router.post("/delete-user/:id", homeController.deleteUser);
     router.get("/update-user/:id", homeController.getUpdateUserPage);
     router.post("/users/update-user", homeController.handleUpdateUser);
+
+    //test api
+    router.get("/api/test-api", apiController.testApi);
+
     return app.use("/", router); // nạp tất cả router vào app
 };
 
--- src/server.js
+++ src/server.js
@@ -9,6 +9,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
+    res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, Content-Type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
 // config view engine
 configViewEngine(app);
 
